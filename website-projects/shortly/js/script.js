664da49bbec42973aa72cdbfd1ad02145c255e58
rvitality <vital.renzjohn@gmail.com>
2022-10-03 12:58:21 +0800

    Add form url input validation

--- website-projects/shortly/js/script.js
+++ website-projects/shortly/js/script.js
@@ -1,1 +1,34 @@
-console.log(123);
+const errorMsg = document.querySelector("#error-msg");
+const urlForm = document.querySelector("#url-form");
+const linkInput = document.querySelector("#link-input");
+
+const validURL = str => {
+    var pattern = new RegExp(
+        "^(https?:\\/\\/)?" + // protocol
+            "((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|" +
+            "((\\d{1,3}\\.){3}\\d{1,3}))" +
+            "(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*" +
+            "(\\?[;&a-z\\d%_.~+=-]*)?" +
+            "(\\#[-a-z\\d_]*)?$",
+        "i"
+    );
+    return !!pattern.test(str);
+};
+
+urlForm.addEventListener("submit", e => {
+    e.preventDefault();
+
+    const inputValue = linkInput.value.trim();
+
+    if (!inputValue) {
+        errorMsg.innerHTML = "Please enter something";
+        linkInput.classList.add("border-red");
+    } else if (!validURL(inputValue)) {
+        errorMsg.innerHTML = "Please enter a valid URL";
+        linkInput.classList.add("border-red");
+    } else {
+        errorMsg.innerHTML = "";
+        linkInput.classList.remove("border-red");
+        alert("Success");
+    }
+});
